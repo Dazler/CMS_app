4bb87cece3d4dd5e90fe354638afce8819209c44
Dazler <tiwarysriavi@gmail.com>
2019-03-07 10:43:10 +0530

    CMS app, first commit

---
+++ CMS_app/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Aircraft(models.Model):
+    MSN = models.IntegerField(default=0)
+    Harness_length = models.IntegerField(default=0)
+    Gross_weight = models.IntegerField(default=0)
+    Atmospheric_pressure = models.IntegerField(default=0)
+    Room_temperature = models.IntegerField(default=0)
+    Airport = models.TextField(default="Heaven")
+    Fuel_Capacity_on_Left_Wing = models.IntegerField(default=0)
+    Fuel_Capacity_on_Right_Wing = models.IntegerField(default=0)
+    Fuel_Quantity_on_Left_Wing = models.IntegerField(default=0)
+    Fuel_Quantity_on_Right_Wing = models.IntegerField(default=0)
+    Maximum_Altitude_to_be_Reached = models.IntegerField(default=0)
+    Flight_No = models.TextField(default="CR7")
